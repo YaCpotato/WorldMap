f767413f923b280f4b716246ad8f6dc4ff5b7187
YaCpotato <yasshisshy@gmail.com>
2020-03-31 21:42:16 +0900

    propsがundefined?

---
+++ src/components/World.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="svg" />
+</template>
+
+<script>
+export default {
+  name: "World",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    svg() {
+      return () => import(`../assets/svg/${this.name}.svg`);
+    }
+  }
+};
+</script>
